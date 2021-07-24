23c1de6e659f9b6d7014d6d8858e89bd6157c62d
Kevlyn Kadamala <kevlyn@gmail.com>
2021-07-24 22:00:06 +0530

    Switched to google

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,12 @@ module.exports = {
   pathPrefix: config.pathPrefix,
   plugins: [
     {
-      resolve: `gatsby-plugin-goatcounter`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        code: isProduction ? 'https://kad99kev-pw.goatcounter.com' : 'https://kad99kev-dev.goatcounter.com',
-        allowLocal: !isProduction,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-CZQBEPLFW4"
+        ],
       },
     },
     {
